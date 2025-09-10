d047479706a5d06b349484040c2d54d9ed7cbeac
Siddeshwar <sidwak6@gmail.com>
2025-09-10 08:31:28 +0000

    configred live metrics

--- app.py
+++ app.py
@@ -1,4 +1,9 @@
 from flask import Flask
+import logging
+from azure.monitor.opentelemetry import configure_azure_monitor
+
+configure_azure_monitor(connection_string="InstrumentationKey=f36037ba-2352-4962-bc52-643c2ef91d14;IngestionEndpoint=https://centralindia-0.in.applicationinsights.azure.com/;LiveEndpoint=https://centralindia.livediagnostics.monitor.azure.com/;ApplicationId=4d3c3dd2-f143-4011-82ea-46c0045326f5",
+                        enable_live_metrics=True)
 
 app = Flask(__name__)
 
