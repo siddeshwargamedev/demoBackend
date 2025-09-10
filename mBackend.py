6f865d67d2d65b610e4bfdc8070ffea82062c3e5
Siddeshwar <sidwak6@gmail.com>
2025-09-10 07:00:43 +0000

    second_commit

--- mBackend.py
+++ mBackend.py
@@ -6,5 +6,11 @@ app = Flask(__name__)
 def hello():
     return 'Hello, this is a demo endpoint'
 
+
+@app.route('/demoerror')
+def hello():
+    raise ValueError('A very specific bad thing happened.')
+    #return 'This endpoint will throw an error'
+
 if __name__ == '__main__':
     app.run(debug=True)
